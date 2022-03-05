4f0143b249c7fe1c3def7438a3751255ca35713d
luke-benham <luke.benham@gmail.com>
2022-03-05 14:45:04 -0500

    Google Login

---
+++ oauth_app/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+# Register your models here.
+# Project name : django-oauth
+# Project ID   : django-oauth-343219
+# Client ID    : 780854667400-0npde56389ke82r5479vkc8if695lvdf.apps.googleusercontent.com
+# Client Secret: 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
+# Superuser 
+# Username     : admin
+# Email        : lnb6grp@virginia.edu 
+# Password     : admin
