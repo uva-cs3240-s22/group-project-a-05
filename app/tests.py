10aa9d98dee8f95dd7337ce3b3f45939c8e40e60
AidanHimley <ah8uqq@virginia.edu>
2022-03-22 21:28:20 -0400

    remove the intentional fail test because I forgot

--- app/tests.py
+++ app/tests.py
@@ -4,6 +4,3 @@ from django.test import TestCase
 class DummyTest(TestCase):
     def testShouldWork(self):
         self.assertTrue(True)
-    
-    def testShouldFail(self):
-        self.assertTrue(False)
