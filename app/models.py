1d287caa97f20ff8d3f96efcc4d9dd5cc0c91c8b
AidanHimley <ah8uqq@virginia.edu>
2022-03-24 22:00:47 -0400

    add recipes to admin page

--- app/models.py
+++ app/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-class recipe (models.Model):
-    name=models.CharField(max_length=400)
-    description=models.CharField(max_length=1000)
-    ingredient=models.CharField(max_length=1000)
-    time=models.CharField(max_length=400)
-    step=models.CharField(max_length=2000)
+class Recipe (models.Model):
+    name        = models.CharField(max_length=400)
+    description = models.CharField(max_length=1000)
+    ingredient  = models.CharField(max_length=1000)
+    time        = models.CharField(max_length=400)
+    step        = models.CharField(max_length=2000)
     def _str_(self):
         return self.name
