87bbb464a27fd93e5092c85b54514c604a60b413
evan-yan <89653888+evan-yan@users.noreply.github.com>
2022-03-25 15:25:50 -0400

    user_profile created

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'app'
 urlpatterns = [
     path('', views.index, name='index'), 
-    path('create', views.create_recipe, name="create_recipe")
+    path('create', views.create_recipe, name="create_recipe"),
+    path('profile', views.userprofile, name="userprofile")
 ]
