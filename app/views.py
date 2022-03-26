6058e0f1dd2f5edfd314b33ab195fd94324a6676
AidanHimley <ah8uqq@virginia.edu>
2022-03-26 17:50:42 -0400

    Add navigation between pages

--- app/views.py
+++ app/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
+from .models import Recipe
 
 # Create your views here.
 def index(request):
-    return render(request, 'app/index.html')
+    return render(request, 'app/index.html', { 'recipes': Recipe.objects.all() })
 
 def create_recipe(request):
     return render(request, 'app/create_recipe.html')
 
 def profile(request):
-    print(request.user.liked_recipes.all())
     return render(request, 'app/userprofile.html')
