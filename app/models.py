e84a6077ab726535c4e78f85603cf2ead01da03a
Megan Cockrell <mec5fy@virginia.edu>
2022-04-12 15:48:14 -0400

    testing

--- app/models.py
+++ app/models.py
@@ -3,13 +3,13 @@ from django.db import models
 from django.contrib.auth import models as authmodels
 # Create your models here.
 
-
 class Comment(models.Model):
     author = models.ForeignKey(authmodels.User, on_delete=models.CASCADE, related_name="comments")
     comment_text=models.CharField(max_length=1000)
 
     def __str__(self):
         return self.comment_text
+
 class Recipe (models.Model):
     name        = models.CharField(max_length=400)
     description = models.CharField(max_length=1000)
