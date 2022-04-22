8baf96ab50db930659f89d5ca43f2cee54d1f1fa
AidanHimley <ah8uqq@virginia.edu>
2022-04-21 23:19:28 -0400

    fixed tests

--- app/tests.py
+++ app/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from .models import Recipe
+from .models import Recipe, Ingredient
 from django.urls import reverse
 
 # Create your tests here.
@@ -9,16 +9,24 @@ class DummyTest(TestCase):
         self.assertTrue(True)
 
 class recipeTest(TestCase):
-    def create_recipe(self, name="Chicken", description="Roasted Chicken", ing="Chicken, Garlic", time="30 minutes", steps="1. Roast Chicken 2. Enjoy"):
+    def create_recipe(self, name="Chicken", description="Roasted Chicken", ing=[], time="30 minutes", steps="1. Roast Chicken 2. Enjoy"):
         test_user = User.objects.create(username='test user')
-        return Recipe.objects.create(name=name, description=description, ingredients=ing, time=time, steps=steps, author=test_user)
+        recipe = Recipe.objects.create(name=name, description=description, time=time, steps=steps, author=test_user)
+        for ingredient in ing:
+            Ingredient.objects.create(recipe=recipe, name=ingredient['name'], amount=ingredient['amount'], units=ingredient['units'])
+        return recipe
 
     def test_create_recipe(self):
-        rep=self.create_recipe()
+        rep=self.create_recipe(ing=[{'name':'Chicken', 'amount':1, 'units':""}, {'name':'Garlic', 'amount':1, 'units':'some'}])
         self.assertTrue(isinstance(rep, Recipe))
         self.assertEqual("Chicken", rep.name)
         self.assertEqual("Roasted Chicken", rep.description)
-        self.assertEqual("Chicken, Garlic", rep.ingredients)
+        self.assertEqual("Chicken", rep.ingredients.all()[0].name)
+        self.assertEqual(1, rep.ingredients.all()[0].amount)
+        self.assertEqual("", rep.ingredients.all()[0].units)
+        self.assertEqual("Garlic", rep.ingredients.all()[1].name)
+        self.assertEqual(1, rep.ingredients.all()[1].amount)
+        self.assertEqual("some", rep.ingredients.all()[1].units)
         self.assertEqual("30 minutes", rep.time)
         self.assertEqual("1. Roast Chicken 2. Enjoy", rep.steps)
 
