c105d7d56c51e2106dc01be9dd48b6eedc3f1783
luke-benham <luke.benham@gmail.com>
2022-04-26 11:29:49 -0400

    DEBUG = True

--- cs3240project/settings.py
+++ cs3240project/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(_+5*f#lj7&u@150q+5q)j$vl_1k5z@mc#latwm$eku--v-u#p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
